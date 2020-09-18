5f6d349dd9c0d31c01a92756de558635269bea2b
Hasan Qasim <hasanqasim92@gmail.com>
2020-09-19 09:34:29 +1000

    Initial Commit

---
+++ BJJRollTrackerTests/BJJRollTrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  BJJRollTrackerTests.swift
+//  BJJRollTrackerTests
+//
+//  Created by Hasan Qasim on 19/9/20.
+//  Copyright © 2020 Hasan Qasim. All rights reserved.
+//
+
+import XCTest
+@testable import BJJRollTracker
+
+class BJJRollTrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
